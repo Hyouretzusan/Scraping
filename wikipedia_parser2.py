3a504f4a828a2f692604f3d849512e849b926e80
unknown <claudioguion@gmail.com>
2018-02-26 11:46:58 -0400

    Segundo commit, modificaciones menores

--- wikipedia_parser2.py
+++ wikipedia_parser2.py
@@ -4,18 +4,20 @@ import datetime
 import random
 import re
 
-random.seed(datetime.datetime.now())
-def buscarLinks(linkUrl):
+random.seed(datetime.datetime.now()) #Inicia el numero random con la hora del pc
+
+def buscarLinks(linkUrl): #Genera el objeto BS, y extrae los links
+    
     html = urlopen("http://en.wikipedia.org" + linkUrl)
     bsObj = BeautifulSoup(html, "html.parser")
     return bsObj.find("div", {"id":"bodyContent"}).findAll("a",\
-    href=re.compile("^(/wiki/)((?!:).)*$"))
+    href=re.compile("^(/wiki/)((?!:).)*$")) #Genera una lista de links
 
 print("Ingrese la parte final del link de wikipedia que desea evaluar")
 linkUsu = input("tal como aparece en su navegador. [Ej: Kevin_Bacon]: ")
 linksWiki = buscarLinks("/wiki/" + linkUsu)
 
-while len(linksWiki) > 0:
+while len(linksWiki) > 0: #Recibe la lista de links y escoge uno al azar
     nuevoLink = linksWiki[random.randint(0, len(linksWiki)-1)].attrs["href"]
     print(nuevoLink)
     linksWiki = buscarLinks(nuevoLink)

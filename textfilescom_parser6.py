20eb7c6c47296349e9dccc367bfcd3ece3547d81
unknown <claudioguion@gmail.com>
2018-03-05 21:05:22 -0400

    Modificaciones mayores. Replanteado el algoritmo para retomar el parseo desde el ultimo archivo de texto registrado

--- textfilescom_parser6.py
+++ textfilescom_parser6.py
@@ -4,106 +4,118 @@ import re
 from textfiles_parsingbdd import textfiles_management
 import sqlite3
 
-listaLink = []
 bDatos = textfiles_management
+conn = sqlite3.connect('tfparsing_bdd.sqlite')
+cur = conn.cursor()
 
-def directoryMap():
+def startScraping():
+    cur.execute('SELECT id FROM Textos')
+    lastText = max([row[0] for row in cur])
+
+    if len(lastText) == 0:
+        directoryMap(0) #Empieza una corrida con la base de datos vacía
+    else:
+        cur.execute('SELECT * FROM Textos WHERE id = ?', (lastText, ))
+        lastTextInfo = cur.fetchone()
+        lastTextNivel = lastTextInfo[2]
+        lastTextPadreId = lastTextInfo[3]
+
+        if lastTextNivel == 1:
+            directoryMap(lastTextPadreId - 1) #Comienza parseo desde último directorio registrado en tabla Textos
+
+        elif lastTextNivel == 2:
+            cur.execute('SELECT link, directorio_id FROM Subdirectorio WHERE id = ?', (lastTextPadreId, ))
+            subdirectorioPadre = cur.fetchone()[0] #link de subdirectorio
+            directorioPadreId = cur.fetchone()[1] #id de directorio
+            cur.execute('SELECT link FROM Directorio WHERE id = ?', (directorioPadreId, ))
+            directorioPadre = cur.fetchone()[0] #link de directorio
+            tuplaSub = (directorioPadre, subdirectorioPadre) #(link directorio, link subdirectorio)
+            subdirectoryText(tuplaSub)
+            directoryMap(directorioPadreId - 1)
+
+
+def directoryMap(corrida): #El parámetro 'corrida' sirve para indicar desde donde se empieza
+    listaPreliminar = []
     html = urlopen("http://www.textfiles.com/directory.html")
     bsObj = BeautifulSoup(html, "html.parser")
     noBuscado = list(bsObj.findAll("a", href=re.compile("(\w+)\.")))
     for link in bsObj.findAll("a", href=re.compile("(\w+)")):
-        if link not in noBuscado:
-            if 'href' in link.attrs:
-                listaLink.append(link.attrs["href"])
-    listaLink.remove("virus")
-    return(bDatos.bdd_insertardirectorio(listaLink), directoryText(listaLink))
-    #return(subdirectoryText(listaLink)) #DEBB
+        if link not in noBuscado and if 'href' in link.attrs:
+            if link.attrs["href"] == "virus":
+                continue
+            listaPreliminar.append(link.attrs['href'])
+    listaLink = listaPreliminar[corrida:] 
+    print("Directorios por parsear:", len(listaLink))
+    directoryText(listaLink)
+    #subdirectoryText(listaLink) #DEBB
 
 
 def directoryText(listaLink): #Directorios de primer nivel
     
-    conn = sqlite3.connect('tfparsing_bdd.sqlite')
-    cur = conn.cursor()
     cur.execute('SELECT link, padre_id FROM Textos')
     guardianText = [row[0] for row in cur]
     guardianTextId = [row[1] for row in cur]
-    cur.execute('SELECT link FROM Subdirectorio')
-    guardianSub = [row[0] for row in cur]
     cur.execute('SELECT link FROM Directorio')
     guardianDir = [row[0] for row in cur]
 
-    final = len(guardianDir) - len(listaLink)
     contador = 0
     
-    for pagina in listaLink:
+    for Dir in listaLink:
 
-        cur.execute('SELECT id FROM Directorio WHERE link = ?', (pagina, ))
-        revision = cur.fetchone()[0]
-        
-        if revision and (revision + 1) in guardianTextId: #Deja pasar el ultimo elemento siempre
-            print("La base de datos ya contiene todos los registros del directorio: %s" % pagina)
+        if Dir in guardianDir:
             continue
 
-        html = urlopen("http://www.textfiles.com/%s" % pagina)
+        html = urlopen("http://www.textfiles.com/%s" % Dir)
         bsObj = BeautifulSoup(html, "html.parser")
-        
+            
         for link in bsObj.findAll("a", href=re.compile("(.txt$)")):
             if 'href' in link.attrs:
-                tuplaPagina= (pagina, link.attrs["href"])
-                if tuplaPagina[1] in guardianText: #Podria dejar pasar la lista, y poner el guardian directamente en bdd_insertartexto 
-                    print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaPagina[1])
+                tuplaDir= (Dir, link.attrs["href"])
+                if tuplaDir[1] in guardianText:
+                    print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaDir[1])
                     continue
-                bDatos.bdd_insertartexto(tuplaPagina, "Directorio")
-        contador += 1
-        print("Directorio %d de %d" % (contador,final))
-
+                bDatos.bdd_insertartexto(tuplaDir, "Directorio")
+        
         for link in bsObj.findAll("a", href=re.compile("^[^/]([A-Z]+[0-9]*[^a-z.//]+)")):
             if 'href' in link.attrs:
-                tuplaSub = (pagina, link.attrs["href"])
+                tuplaSub = (Dir, link.attrs["href"]) #(link directorio, link subdirectorio)
                 subdirectoryText(tuplaSub)
-                if tuplaSub[1] in guardianSub: #Podria dejar pasar la lista, y poner el guardian directamente en bdd 
-                    print("El subdirectorio %s ya se encuentra registrado en la base de datos." % tuplaSub[1])
-                    continue
-                bDatos.bdd_insertarsubdirectorio(tuplaSub)
-    conn.close()
-    #return(bDatos.bdd_insertartexto(listaPagina, "Directorio"), bDatos.bdd_insertarsubdirectorio(listaSub), subdirectoryText(listaSub))
-    #print(listaPagina) #DEBB 
+
+        bDatos.bdd_insertardirectorio(Dir)
+        contador += 1
+        print("Directorio %d de %d" % (contador, len(listaLink)))
 
 
 def subdirectoryText(tuplaSub): #Directorios de segundo nivel. (tupla[0] es padre, tupla[1] es hijo)
-
-    conn = sqlite3.connect('tfparsing_bdd.sqlite')
-    cur = conn.cursor()
+    
+    cur.execute('SELECT link FROM Subdirectorio')
+    guardianSub = [row[0] for row in cur]
     cur.execute('SELECT link FROM Textos')
     guardianText = [row[0] for row in cur]
     contador = 0
     padre = tuplaSub[0]
     hijo = tuplaSub[1]
-    html = urlopen("http://www.textfiles.com/%s/%s" % tuplaSub)
-    bsObj = BeautifulSoup(html, "html.parser")
-        
-    for link in bsObj.findAll("a", href=re.compile("(.txt$)")):
-        if 'href' in link.attrs:
-            tuplaPagina= (hijo, link.attrs["href"])
-            if tuplaPagina[1] in guardianText: #Podria dejar pasar la lista, y poner el guardian directamente en bdd_insertartexto 
-                print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaPagina[1])
-                continue
-            bDatos.bdd_insertartexto(tuplaPagina, "Subdirectorio")
-        
-        contador += 1
-        print("Subdirectorio %d" % contador)       
-    conn.close()     
+
+    if hijo in guardianSub: 
+        print("El subdirectorio %s ya se encuentra registrado en la base de datos." % tuplaSub[1])
     
-    #return(bDatos.bdd_insertartexto(listaPagina, "Subdirectorio"))
-    #print(listaPagina) #DEBB 
+    else: 
+        html = urlopen("http://www.textfiles.com/%s/%s" % tuplaSub)
+        bsObj = BeautifulSoup(html, "html.parser")
+            
+        for link in bsObj.findAll("a", href=re.compile("(.txt$)")):
+            if 'href' in link.attrs:
+                tuplaPagina= (hijo, link.attrs["href"])
+                
+                if tuplaPagina[1] in guardianText:
+                    print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaPagina[1])
+                    continue
+                bDatos.bdd_insertartexto(tuplaPagina, "Subdirectorio")
+            
+        contador += 1
+        print("Subdirectorio %d" % contador)
+        bDatos.bdd_insertarsubdirectorio(tuplaSub)
 
-directoryMap()
+conn.close()  
 
-"""Debo realizar el algoritmo de manera que empiece en un directorio, vaya guardando los textos de ese directorio por un lado
-y los subdirectorios del texto por otro. Teniendo los subdirectorios, vaya recorriendo uno por uno para ir guardando sus respectivos
-textos. Los registros se irán guardando en la base de datos a medida que se generan y, cuando quiera reiniciar el parser, deberá
-consultar en la base de datos para determinar en cuál fue el último directorio en que se quedó, y empezar desde cero en ese directorio, 
-incluso si ya lo había recorrido casi todo anteriormente. Eso evitará que se me escapen registros en caso de un paro inesperado. Para evitar
-duplicidad, en este algoritmo, los guardianes estarán implementados justo en el momento antes de insertar el registro en la base de datos
-
-REHACER EL PARSER PARA QUE SOLO PASE AL SIGUIENTE DIRECTORIO SI YA RECORRIÓ TODO EL DIRECTORIO ANTERIOR, INCLUÍDOS LOS SUBDIRECTORIOS"""
+startScraping()

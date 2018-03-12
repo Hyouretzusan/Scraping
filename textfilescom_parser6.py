bbbca3eeccafe6588596146142530ed055ea0d7b
unknown <claudioguion@gmail.com>
2018-03-12 08:11:35 -0400

    Correcciones menores

--- textfilescom_parser6.py
+++ textfilescom_parser6.py
@@ -60,7 +60,7 @@ def directoryText(listaLink): #Directorios de primer nivel
 
     contador = 0
     
-    for Dir in listaLink:
+    for Dir in listaLink: #Por cada directorio en la lista recibida, se ejecuta el siguiente codigo
 
         if Dir in guardianDir:
             continue
@@ -74,22 +74,22 @@ def directoryText(listaLink): #Directorios de primer nivel
                 if tuplaDir[1] in guardianText:
                     print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaDir[1])
                     continue
-                bDatos.bdd_insertartexto(tuplaDir, "Directorio")
+                bDatos.bdd_insertartexto(tuplaDir, "Directorio") #Genera los links para txt de directorios uno por uno, y los pasa
         
         for link in bsObj.findAll("a", href=re.compile("^[^/]([A-Z]+[0-9]*[^a-z.//]+)")):
             if 'href' in link.attrs:
                 tuplaSub = (Dir, link.attrs["href"]) #(link directorio, link subdirectorio)
-                subdirectoryText(tuplaSub)
+                subdirectoryText(tuplaSub) #Genera los links para subdirectorios uno por uno, y los pasa
 
-        bDatos.bdd_insertardirectorio(Dir)
+        bDatos.bdd_insertardirectorio(Dir) #Solo guardará un directorio en bdd cuando ambos for anteriores se cumplan
         contador += 1
-        print("Directorio %d de %d" % (contador, len(listaLink)))
+        print("Directorio %d de %d" % (contador, len(listaLink))) 
 
 
-def subdirectoryText(tuplaSub): #Directorios de segundo nivel. (tupla[0] es padre, tupla[1] es hijo)
+def subdirectoryText(tuplaSub): #Subdirectorios. (tupla[0] es directorio, tupla[1] es subdirectorio)
     
     cur.execute('SELECT link FROM Subdirectorio')
-    guardianSub = [row[0] for row in cur]
+    guardianSub = [row[0] for row in cur] 
     cur.execute('SELECT link FROM Textos')
     guardianText = [row[0] for row in cur]
     contador = 0
@@ -110,11 +110,11 @@ def subdirectoryText(tuplaSub): #Directorios de segundo nivel. (tupla[0] es padr
                 if tuplaPagina[1] in guardianText:
                     print("El archivo %s ya se encuentra registrado en la base de datos." % tuplaPagina[1])
                     continue
-                bDatos.bdd_insertartexto(tuplaPagina, "Subdirectorio")
+                bDatos.bdd_insertartexto(tuplaPagina, "Subdirectorio") 
             
         contador += 1
         print("Subdirectorio %d" % contador)
-        bDatos.bdd_insertarsubdirectorio(tuplaSub)
+        bDatos.bdd_insertarsubdirectorio(tuplaSub) #Solo guarda en bdd cuando haya recorrido todos los txt dentro del sub
 
 conn.close()  
 

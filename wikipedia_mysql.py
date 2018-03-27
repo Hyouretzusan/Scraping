d1dfa1023404348e6391e88f9f997364b1fe2111
unknown <claudioguion@gmail.com>
2018-03-26 22:16:46 -0400

    wikipedia_mysql.py c1: Script para leer links de paginas wikipedia y accesarlos de forma aleatoria. Se guarda el título y el primer párrafo de cada pagina en bdd

--- wikipedia_mysql.py
+++ wikipedia_mysql.py
@@ -3,31 +3,37 @@ from bs4 import BeautifulSoup
 import datetime
 import random
 import pymysql
+import re
 
 conn = pymysql.connect(host='127.0.0.1', user='claudio', passwd="sonicboom", db='scraping', charset='utf8')
 cur = conn.cursor()
 random.seed(datetime.datetime.now())
 
 def store(title, content):
+    print("Storing:", title, content)
     cur.execute("INSERT INTO pages (title, content) VALUES (\"%s\",\"%s\")", (title, content))
     cur.connection.commit()
 
 def getLinks(articleUrl):
+    print("Getting links")
     html = urlopen("http://en.wikipedia.org"+articleUrl)
     bsObj = BeautifulSoup(html, "html.parser")
-    title = bsObj.find("h1").find("span").get_text()
+    title = bsObj.find("h1", {"id":"firstHeading"}).get_text()
     content = bsObj.find("div", {"id":"mw-content-text"}).find("p").get_text()
+    print(title, type(title), content, type(content)) #DEBB
     store(title, content)
     return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 
 links = getLinks("/wiki/Kevin_Bacon")
+linkst = type(links)
+linksl = len(links)
+print("links tipo %s, len %d" % (linkst, linksl))
 try:
     while len(links) > 0:
+        print("New article")
         newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
         print(newArticle)
         links = getLinks(newArticle)
 finally:
     cur.close()
     conn.close()
-
-
